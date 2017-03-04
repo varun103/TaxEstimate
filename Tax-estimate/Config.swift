1e0bf8d85328e17d0b15519ec67c1e646365d844
Varun <vajmera@salesforce.com>
2017-03-04 03:34:17 -0800

    ui changes

--- Tax-estimate/Config.swift
+++ Tax-estimate/Config.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class Config {
     
-    private static let fontType = "Avenir Next Medium"
+    private static let fontType = "AvenirNext-Medium"
     
     // NAVIGATION BAR
     static let navigationBarColor:UIColor =  UIColor(red: 35.0/255.0, green: 220.0/255.0, blue: 147.0/255.0, alpha: 90.0/255.0)
